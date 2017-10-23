13a116b5aa9c0d2fd2c40d8b02a590ac58defc92
Jiangjun-2016 <295392867@qq.com>
2017-10-23 15:47:34 +0800

    下载 线程

---
+++ more/src/main/test/testcode/TestJunit.java
@@ -0,0 +1,46 @@
+package testcode;
+
+import code.controller.UploadController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class) // 使用junit4进行测试
+@ContextConfiguration(locations = {"classpath*:spring-mvc.xml", "classpath*:spring-mybatis.xml"})
+public class TestJunit {
+
+	// 模拟request,response
+	private MockHttpServletRequest request;
+	private MockHttpServletResponse response;
+
+	// 注入Controller
+	@Autowired
+	private UploadController uploadController;
+
+	// 执行测试方法之前初始化模拟request,response
+	@org.junit.Before
+	public void setUp() {
+		request = new MockHttpServletRequest();
+		request.setCharacterEncoding("UTF-8");
+		response = new MockHttpServletResponse();
+	}
+
+	/**
+	 * 测试ftpDownloadFile
+	 */
+	@Test
+	public void ftpDownloadFileTest() {
+		try {
+			request.setParameter("eventId", "1");
+			request.setParameter("documetID", "1708090019001001");
+			uploadController.ftpDownloadFile(request);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
