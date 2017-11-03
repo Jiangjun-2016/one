91b48447a682ecd6fc89e9694f9b41e77da50134
Jiangjun-2016 <295392867@qq.com>
2017-11-03 12:00:34 +0800

    webupload

--- more/src/main/webapp/pages/upload/WebUpload.jsp
+++ more/src/main/webapp/pages/upload/WebUpload.jsp
@@ -1,21 +1,23 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-         pageEncoding="UTF-8" %>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
     <meta http-equiv="Content-Language" content="ja"/>
     <meta http-equiv="Content-Script-Type" content="text/javascript"/>
     <meta http-equiv="Content-Style-Type" content="text/css"/>
     <title>使用webuploader上传</title>
-    <!-- 1.引入文件 -->
+    <link href="../../css/stream-v1.css" rel="stylesheet" type="text/css"/>
     <link href="../../js/webuploader-0.1.5/dist/webuploader.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="../../js/jquery/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="../../js/bootstrap/bootstrap-datepicker.min.js"></script>
+    <script type="text/javascript" src="../../js/bootstrap/bootstrap-datepicker.ja.min.js"></script>
     <script type="text/javascript" src="../../js/webuploader-0.1.5/dist/webuploader.js"></script>
-    <script type="text/javascript" src="../../js/jquery-2.1.4/jquery.min.js"></script>
-    <!-- 3.添加js代码 -->
+    <script type="text/javascript" src="../../js/webuploader-0.1.5/dist/webuploader.min.js"></script>
     <script type="text/javascript" src="../../pages/upload/js/webupload.js"></script>
 </head>
 <body>
-<!-- 2.创建页面元素 -->
 <div id="uploader" class="wu-example">
     <!--用来存放文件信息-->
     <div id="thelist" class="uploader-list"></div>
@@ -24,8 +26,6 @@
         <input type="button" value="上传" id="upload"/>
     </div>
 </div>
-
-
 <div id="uploader1" class="wu-example">
     <!--用来存放文件信息-->
     <div id="thelist1" class="uploader-list"></div>
--- more/src/main/webapp/pages/upload/js/webupload.js
+++ more/src/main/webapp/pages/upload/js/webupload.js
@@ -1,5 +1,6 @@
 /*********************************WebUpload 单文件上传 begin*****************************************/
 $(function () {
+    alert("***");
     var $list = $("#thelist");
     var uploader;// 实例化
     uploader = WebUploader.create({
@@ -86,6 +87,157 @@ $(function () {
 });
 /*********************************WebUpload 单文件上传 end*******************************************/
 
+/*********************************WebUpload 多文件上传 begin*****************************************/
+$(function () {
+    var $list = $("#thelist1");
+    var fileSize = 0;  //总文件大小
+    var fileName = []; //文件名列表
+    var fileSizeOneByOne = [];//每个文件大小
+    var uploader;// 实例化
+    uploader = WebUploader.create({
+        auto: false, //是否自动上传
+        pick: {
+            id: '#multi',
+            label: '点击选择文件',
+            name: "multiFile"
+        },
+        swf: '../../main/js/Uploader.swf',
+        fileVal: 'multiFile',              //和name属性配合使用
+        server: "Webuploader!ajaxAttachUpload2.action",
+        duplicate: true, //同一文件是否可重复选择
+        resize: false,
+        formData: {
+            "status": "multi",
+            "contentsDto.contentsId": "0000004730",
+            "uploadNum": "0000004730",
+            "existFlg": 'false'
+        },
+        compress: null,//图片不压缩
+        chunked: true,  //分片
+        chunkSize: 5 * 1024 * 1024,   //每片5M
+        chunkRetry: false,//如果失败，则不重试
+        threads: 1,//上传并发数。允许同时最大上传进程数。
+        //fileNumLimit:50,//验证文件总数量, 超出则不允许加入队列
+        // runtimeOrder: 'flash',
+        // 禁掉全局的拖拽功能。这样不会出现图片拖进页面的时候，把图片打开。
+        disableGlobalDnd: true
+    });
+
+    // 当有文件添加进来的时候
+    uploader.on("fileQueued", function (file) {
+        console.log("fileQueued:");
+        $list.append("<div id='" + file.id + "' class='item'>" +
+            "<h4 class='info'>" + file.name + "</h4>" +
+            "<p class='state'>等待上传...</p>" +
+            "</div>");
+    });
+
+    // 当开始上传流程时触发
+    uploader.on("startUpload", function () {
+        console.log("startUpload");
+        //添加额外的表单参数
+        $.extend(true, uploader.options.formData, {
+            "fileSize": fileSize,
+            "multiFileName": fileName,
+            "fileSizeOneByOne": fileSizeOneByOne
+        });
+    });
+
+    //当某个文件上传到服务端响应后，会派送此事件来询问服务端响应是否有效。
+    uploader.on("uploadAccept", function (object, ret) {
+        //服务器响应了
+        //ret._raw  类似于 data
+        console.log("uploadAccept");
+        console.log(ret);
+        var data = JSON.parse(ret._raw);
+        if (data.resultCode != "1" && data.resultCode != "3") {
+            if (data.resultCode == "9") {
+                alert("error");
+                uploader.reset();
+                return;
+            }
+        } else {
+            uploader.reset();
+            alert("error");
+        }
+    })
+
+    uploader.on("uploadSuccess", function (file) {
+        $("#" + file.id).find("p.state").text("已上传");
+    });
+
+    //出错之后要把文件从队列中remove调，否则，文件还在队里中，还是会上传到后台去
+    uploader.on("uploadError", function (file, reason) {
+        $("#" + file.id).find("p.state").text("上传出错");
+        console.log("uploadError");
+        console.log(file);
+        console.log(reason);
+        //多个文件
+        var fileArray = uploader.getFiles();
+        for (var i = 0; i < fileArray.length; i++) {
+            //取消文件上传
+            uploader.cancelFile(fileArray[i]);
+            //从队列中移除掉
+            uploader.removeFile(fileArray[i], true);
+        }
+        //发生错误重置webupload,初始化变量
+        uploader.reset();
+        fileSize = 0;
+        fileName = [];
+        fileSizeOneByOne = [];
+    });
+
+    //当validate不通过时，会以派送错误事件的形式通知调用者
+    uploader.on("error", function () {
+        console.log("error");
+        uploader.reset();
+        fileSize = 0;
+        fileName = [];
+        fileSizeOneByOne = [];
+        alert("error");
+    })
+
+
+    //如果是在模态框里的上传按钮，点击file的时候不会触发控件
+    //修复model内部点击不会触发选择文件的BUG
+    /*    $("#multi .webuploader-pick").click(function () {
+     uploader.reset();
+     fileSize = 0;
+     fileName = [];
+     fileSizeOneByOne=[];
+     $("#multi :file").click();
+     });*/
+
+    /**
+     * 多文件上传
+     */
+    $("#multiUpload").on("click", function () {
+        uploader.upload();
+    })
+
+    /**
+     *取得每个文件的文件名和文件大小
+     */
+    //选择文件之后执行上传
+    $(document).on("change", "input[name='multiFile']", function () {
+        //multiFileName
+        var fileArray1 = uploader.getFiles();
+        var fileNames = [];
+        for (var i = 0; i < fileArray1.length; i++) {
+            fileNames.push(fileArray1[i].name); //input 框用
+            //后台用
+            fileSize += fileArray1[i].size;
+            fileSizeOneByOne.push(fileArray1[i].size);
+            fileName.push(fileArray1[i].name);
+        }
+        console.log(fileSize);
+        console.log(fileSizeOneByOne);
+        console.log(fileName);
+    })
+
+});
+/*********************************WebUpload 多文件上传 end*****************************************/
+
 /************************************webuploader的自带参数提交到后台的参数列表*************************
  * {
 
@@ -99,4 +251,12 @@ size=[268620636], name=[3.wav],
 contentsDto.contentsId=[0000004730], existFlg=[false],
 status=[file], uploadNum=[0000004730]
 }
+
+ 可以用这个方法打印webuploader的自带参数列表，基本上就是上面的那一些
+ //当某个文件的分块在发送前触发
+ uploader.on("uploadBeforeSend",function(object ,data){
+   console.log("uploadBeforeSend");
+   console.log(object);
+   console.log(data);
+ })
  *********************************************************************************************/
