9706aafab4deecf524c4aaa0fb06fad9829cdd11
Jiangjun-2016 <295392867@qq.com>
2017-11-10 13:57:59 +0800

    cxf client test

--- more/src/main/test/testcode/TestWebService.java
+++ more/src/main/test/testcode/TestWebService.java
@@ -1,7 +1,11 @@
 package testcode;
 
 import codespring.service.WebServerService;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 
 /**
  * cxf webservice测试
@@ -12,6 +16,19 @@ import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 
 public class TestWebService {
 
+	public static final int CXF_CLIENT_CONNECT_TIMEOUT = 50 * 1000;
+	public static final int CXF_CLIENT_RECEIVE_TIMEOUT = 100 * 1000;
+
+	public static void configTimeout(Object service) {
+		Client proxy = ClientProxy.getClient(service);
+		HTTPConduit conduit = (HTTPConduit) proxy.getConduit();
+		HTTPClientPolicy policy = new HTTPClientPolicy();
+		policy.setConnectionTimeout(CXF_CLIENT_CONNECT_TIMEOUT);
+		policy.setReceiveTimeout(CXF_CLIENT_RECEIVE_TIMEOUT);
+		conduit.setClient(policy);
+	}
+
+
 	public static void main(String[] args) {
 
 		JaxWsProxyFactoryBean webService = new JaxWsProxyFactoryBean();
@@ -19,6 +36,7 @@ public class TestWebService {
 		webService.setAddress("http://localhost:8088/one/services/oneWebServer");
 		WebServerService webServerService = (WebServerService) webService
 				.create();
+		configTimeout(webServerService);
 		String info = webServerService.getUsers("1");
 		System.out.println(info);
 
