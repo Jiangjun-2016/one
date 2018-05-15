9a36dd2989eeee0c93a72c3280ac9b05ac7d2693
fan <295392867@qq.com>
2018-05-15 09:50:27 +0800

    InitializingBeanDemo

---
+++ myUtil/src/main/java/spring/InitializingBeanDemo.java
@@ -0,0 +1,16 @@
+package spring;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author fxf
+ * @create 2018-05-15 9:46
+ **/
+@Component
+public class InitializingBeanDemo implements InitializingBean {
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("容器启动成功后执行此方法。");
+	}
+}
